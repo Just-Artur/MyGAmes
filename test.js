e86ee79e559f5c53cca9d4f08f66db17d49d9404
ArturSuperpizza <129962627+ArturSuperpizza@users.noreply.github.com>
2023-04-05 14:01:56 +0100

    Add files via upload

---
+++ test.js
@@ -0,0 +1,7 @@
+console.log('Hiiiii, Why are you inspecting?'); String
+
+function button(){
+    x = document.getElementById("button")
+    x.innerHTML = "Click this Button Now!!!!"
+    x.innerHTML = "You fell in my TRAP YOU FOOL!!!!"
+}
