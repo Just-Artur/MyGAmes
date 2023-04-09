a16d531dc3029838d85e95bdb4b5c6146f6415de
ArturSuperpizza <129962627+ArturSuperpizza@users.noreply.github.com>
2023-04-09 14:37:57 +0100

    Update test.js

--- test.js
+++ test.js
@@ -3,7 +3,6 @@ console.log('Hiiiii, Why are you inspecting?'); String
 function button(){
     x = document.getElementById("button")
     x.innerHTML = "Click this Button Now!!!!"
-    x.inne
-rHTML = "You fell in my TRAP YOU FOOL!!!!"
+    x.innerHTML = "You fell in my TRAP YOU FOOL!!!!"
     alert('You got FOOLED!!!');
 }
