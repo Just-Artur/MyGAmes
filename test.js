8dcdf9d8d1b6e695e86d55178cb902a5f1fd018c
ArturSuperpizza <129962627+ArturSuperpizza@users.noreply.github.com>
2023-04-09 14:51:57 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 console.log('Hiiiii, Why are you inspecting?'); String
-alert('You got FOOLED!!!');
 
 function button(){
+    alert('You got Trolled you FOOL!!! Press OK to continue...')
     x = document.getElementById("button")
     x.innerHTML = "Click this Button Now!!!!"
     x.innerHTML = "You fell in my TRAP YOU FOOL!!!!"  
